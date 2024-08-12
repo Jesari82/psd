a35c67c727438c3ebe03f754bfcc18f6bc4f442c
Denis <ddziubek.11@gmail.com>
2024-08-12 15:16:08 +0200

    onepager

--- src/components/Mainlayout.jsx
+++ src/components/Mainlayout.jsx
@@ -1,13 +1,31 @@
 import Footer from "./Footer.jsx";
 import Navbar from "./Navbar.jsx"
 import { Outlet } from "react-router-dom"
+import Home from "./Home.jsx"
+import About from "./About.jsx"
+import Process from "./Process.jsx"
+import Portfolio from "./Portfolio.jsx"
+import Blog from "./Blog.jsx"
+import Services from "./Services.jsx"
+import Contact from "./Contact.jsx"
+import CallToAction from "./CallToAction.jsx";
+import HappyClients from "./HappyClients.jsx";
 
 function Mainlayout() {
     return (
         <div>
            <Navbar />
            <div>
-                <Outlet />
+                <Home/>
+                <About/>
+                <Process/>
+                <Portfolio/>
+                <CallToAction/>
+                <Blog/>
+                <Services/>
+                
+                <Contact/>
+
            </div> 
            <Footer /> 
         </div>
