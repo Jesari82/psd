3ca433b3ef2b0f00c6f19a2f850f7ff366c7a2cd
Ahmed Mohamad <rashin2604@gmail.com>
2024-08-12 12:11:17 +0200

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import {Link} from 'react-router-dom';
 function Navbar () {
   return (
-    <nav>
-        <div className="logo">
-            <span>B</span>
+    <nav className='flex justify-between'>
+        <div>
+            <span className='bg-purple-500 rounded-full p-3 text-xl mr-3'>B</span>
             <span>Broklyn</span>
         </div>
-        <div className="menu">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/process">Process</Link>
-            <Link to="/portfolio">Portfolio</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/services">Services</Link>
-            <Link to="/contact">Contact</Link>
+        <div className="flex justify-between gap-3">
+            <Link to="/" className='hover:bg-purple-500 hover:rounded-md'>Home</Link>
+            <Link to="/about" className='hover:bg-purple-500 hover:rounded-md'>About</Link>
+            <Link to="/process" className='hover:bg-purple-500 hover:rounded-md'>Process</Link>
+            <Link to="/portfolio" className='hover:bg-purple-500 hover:rounded-md'>Portfolio</Link>
+            <Link to="/blog" className='hover:bg-purple-500 hover:rounded-md'>Blog</Link>
+            <Link to="/services" className='hover:bg-purple-500 hover:rounded-md'>Services</Link>
+            <Link to="/contact" className='hover:bg-purple-500 hover:rounded-md'>Contact</Link>
         </div> 
     </nav>
   );
